2f3beeba5efc370a2fbbe3339901f2e74b7b6622
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-04-09 16:16:58 +0200

    :fire: Remove files

--- test/experiment/search.dart
+++ test/experiment/search.dart
@@ -1,9 +1,12 @@
-import 'package:github/github.dart';
+import 'package:github_flutter/github.dart';
 
 void main() {
   final github = GitHub();
 
-  github.search.repositories('github').listen((repo) {
-    print('${repo.fullName}: ${repo.description}');
-  }).onDone(github.dispose);
+  github.search
+      .repositories('github')
+      .listen((repo) {
+        print('${repo.fullName}: ${repo.description}');
+      })
+      .onDone(github.dispose);
 }
